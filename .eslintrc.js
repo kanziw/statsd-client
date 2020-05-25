29eac1e8ca5b5e53862b394751ea4082c5a44861
kanziw <kanziwoong@gmail.com>
2020-05-25 21:25:16 +0900

    Set typescript config up

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: [
+    'eslint-plugin-import-order-alphabetical',
+    'jest',
+    '@typescript-eslint',
+  ],
+  rules: {
+    semi: [ 'error', 'never' ],
+    'max-len': [ 'error', { code: 120, ignoreUrls: true } ],
+    'key-spacing': [ 'error', { afterColon: true, beforeColon: false } ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'comma-spacing': [ 'error', { before: false, after: true } ],
+    'import-order-alphabetical/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          [ 'builtin', 'external' ],
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+  },
+}
